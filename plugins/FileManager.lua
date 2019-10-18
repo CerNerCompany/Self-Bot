a365037a55d5b25f33870198787e1f828b67a117
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-10-19 02:50:24 +0330

    Update FileManager.lua

--- plugins/FileManager.lua
+++ plugins/FileManager.lua
@@ -9,6 +9,7 @@ patterns_ = {
 "^(stickers)$",
 "^(clean files)$",
 "^(clean chats)$",
+"^(upload)$",
 
 "^(clean stickers)$",
 "^(clean gifs)$",
@@ -38,7 +39,7 @@ FileManager = function(msg,crco)
   
     sive = getSixe(tonumber(sixe))
     local type = Get('FileManager:TypeFile:'..value) or 'UnSupported'
-    text = text..key.." - *|"..value.."|*  *Type* `|"..type.."|`*"..sive.."*\n" 
+    text = text..key.." - *|"..value.."|*  *Type* `|"..type.."|`*"..sive.."* [Receive in a chat](https://t.me/share/url?url=get "..value..")\n" 
     end
     if #files == 0 then
     text = 'Message `:` *Empty*'
@@ -107,8 +108,12 @@ is_saved = 1
         end
         tdbot.getMessage(msg.chat_id, tonumber(msg.reply_to_message_id),getDataMessage,nil)
         end
-    ---
+    --- 
    
+   if crco[1] == 'bioadd' then
+    bio = crco[2] 
+  sadd('MainBio')
+  end
     if crco[1] == 'get' then
        Size = Get("FileManager:FileSize:"..crco[2]) or 100
        caption ='*File name* `:` *|'..(crco[2])..'|*\n*File Size* `:` *|'..getSixe(Size)..'|*'
@@ -169,55 +174,55 @@ is_saved = 1
          del("FileManager:Contact:Name:"..crco[2])
          
        else
-         return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *File* :`'..ec_name(crco[2])..'` * is Not Found *','md',false, 0, nil, nil, nil)
+         return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *File* :`'..ec_name(crco[2])..'` * is Not Found *','md',false, 0, nil, nil, nil)
        end
        if Del then 
         sremove('FileManager:Files:',crco[2])
-        return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *File* :`'..ec_name(crco[2])..'` * Removed ! *','md',false, 0, nil, nil, nil)
+        return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *File* :`'..ec_name(crco[2])..'` * Removed ! *','md',false, 0, nil, nil, nil)
 
        end
        end
   if crco[1] == 'setchat' and crco[2] and crco[3] then
         sadd('Chats:',crco[2])
         save('Chats:Name:'..crco[2],crco[3])
-        text = 'Message : *Answer for* (`'..crco[2]..'`) *was set to * `:` (`'..crco[3]..'`)'
+        text = '☤Message : *Answer for* (`'..crco[2]..'`) *was set to * `:` (`'..crco[3]..'`)'
         return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 
         end
    if crco[1] == 'delchat' and crco[2] then
     sremove('Chats:',crco[2])
     del('Chats:Name:'..crco[2])
-           text = 'Message : *Answer* (`'..crco[2]..'`) *was removed *'
+           text = '☤ Message : *Answer* (`'..crco[2]..'`) *was removed *'
            return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 
            end
       if crco[1] == 'chats' then
            chats = Get('Chats:') or {}
-          local  text= 'Chats: \n'
+          local  text= '*☤ Chats ☤*\n'
           for key,value in pairs(chats) do
               local answer = Get("Chats:Name:"..value) or ''
               text = text..key.." - `"..value.."` Anwser : `"..answer.."`\n" 
           end
           if #chats == 0 then
-            text = 'Message `:` *Empty*'
+            text = '☤ Message `:` *Empty*'
           end
           return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
           end
           if crco[1] == 'clean chats' then
             va = Get('Chats:') or {}
-            text = 'Message : *Chats Cleared !*'
+            text = '☤ Message : *Chats Cleared !*'
            for k,v in pairs(va) do
              del("Chats:Name:"..v)
            del('Chats:')
            end
            if #va == 0 then
-           text = 'Message : *Empty*'
+           text = '☤ Message : *Empty*'
            end
             return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
            end
 if crco[1] == 'clean files' then
  files = Get('FileManager:Files:') or {}
- text = 'Message : *DataBase Cleared !*'
+ text = '☤ Message : *DataBase Cleared !*'
 for k,v in pairs(files) do
   del("FileManager:Contact:"..v)
 del("FileManager:Contact:Name:"..v)
@@ -234,7 +239,7 @@ del("FileManager:Photo:"..v)
 del('FileManager:Files:')
 end
 if #files == 0 then
-text = 'Message : *Empty*'
+text = '☤ Message : *Empty*'
 end
  return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 end
@@ -245,7 +250,7 @@ for key,value in pairs(GifsCloud) do
 text = text..key.." - `"..value.."`\n" 
 end
 if #GifsCloud == 0 then
-text = 'Message : *Empty*'
+text = '☤ Message : *Empty*'
 end
  return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 end
@@ -256,7 +261,7 @@ for key,value in pairs(GifsCloud) do
 text = text..key.." - `"..value.."`\n" 
 end
 if #GifsCloud == 0 then
-text = 'Message : *Empty*'
+text = '☤ Message : *Empty*'
 end
  return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 end
@@ -266,10 +271,10 @@ if co.content['@type'] == 'messageAnimation' then
 local gif = co.content.animation.animation.remote.id
 save('Gifs:'..crco[2],gif)
 sadd('Gifs:Cloud:',crco[2])
-        return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Gif seted to cmd* `:` '..crco[2],'md',false, 0, nil, nil, nil)
+        return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Gif seted to cmd* `:` '..crco[2],'md',false, 0, nil, nil, nil)
 
 else
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Gifs only*','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Gifs only*','md',false, 0, nil, nil, nil)
 end
 end
 tdbot.getMessage(msg.chat_id, tonumber(msg.reply_to_message_id),convertcmd,nil)
@@ -278,41 +283,41 @@ if crco[1] == 'gifdel' then
 if CHECK('Gifs:Cloud:',crco[2]) then
 del('Gifs:'..crco[2])
 sremove('Gifs:Cloud:',crco[2])
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message `:` *Gif CMD* `:` |'..crco[2]..'| *Has been Deleted* !','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message `:` *Gif CMD* `:` |'..crco[2]..'| *Has been Deleted* !','md',false, 0, nil, nil, nil)
 else
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Gif CMD  is not found*','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Gif CMD  is not found*','md',false, 0, nil, nil, nil)
 end
 end
 if crco[1] == 'stickerdel' then
 if CHECK('Stickers:Cloud:',crco[2]) then
 del('Stickers:'..crco[2])
 sremove('Stickers:Cloud:',crco[2])
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message `:` *Sticker CMD* `:` |'..gif..'| *Has been Deleted* !','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message `:` *Sticker CMD* `:` |'..gif..'| *Has been Deleted* !','md',false, 0, nil, nil, nil)
 else
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Sticker CMD is not found*','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Sticker CMD is not found*','md',false, 0, nil, nil, nil)
 end
 end
 if crco[1] == 'clean stickers' then
  files = Get('Stickers:Cloud:') or {}
- text = 'Message : *Stickers Cleared !*'
+ text = '☤ Message : *Stickers Cleared !*'
 for k,v in pairs(files) do
   del("Stickers:"..v)
 del('Stickers:Cloud:')
 end
 if #files == 0 then
-text = 'Message : *Empty*'
+text = '☤ Message : *Empty*'
 end
  return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 end
 if crco[1] == 'clean gifs' then
  files = Get('Gifs::Cloud:') or {}
- text = 'Message : *Gifs Cleared !*'
+ text = '☤ Message : *Gifs Cleared !*'
 for k,v in pairs(files) do
   del("Gifs::"..v)
 del('Gifs:Cloud:')
 end
 if #files == 0 then
-text = 'Message : *Empty*'
+text = '☤ Message : *Empty*'
 end
  return tdbot.editMessageText(msg.chat_id, msg.id,text,'md',false, 0, nil, nil, nil)
 end
@@ -321,10 +326,10 @@ convertcmd = function(arg,co)
 if co.content['@type'] == 'messageSticker' then
 save('Stickers:'..crco[2],co.content.sticker.sticker.remote.id)
 sadd('Stickers:Cloud:',crco[2])
-        return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Sticker seted to cmd* `:` '..crco[2],'md',false, 0, nil, nil, nil)
+        return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Sticker seted to cmd* `:` '..crco[2],'md',false, 0, nil, nil, nil)
 
 else
- return tdbot.editMessageText(msg.chat_id, msg.id,'Message : *Stickers only*','md',false, 0, nil, nil, nil)
+ return tdbot.editMessageText(msg.chat_id, msg.id,'☤ Message : *Stickers only*','md',false, 0, nil, nil, nil)
 end
 end
 tdbot.getMessage(msg.chat_id, tonumber(msg.reply_to_message_id),convertcmd,nil)
@@ -335,7 +340,7 @@ end
 PreMessage = function(msg,fast_update)
 if msg then
   if msg.content._ == "messageText" and  msg.content.text.text then
-  if Get('chat:mod') and  Get("Chats:Name:"..msg.content.text.text)  and not is_sudo(msg.sender_user_id)  then
+  if not Get('chat:mod'..msg.chat_id) and  Get("Chats:Name:"..msg.content.text.text)  and not is_sudo(msg.sender_user_id)  then
    return tdbot.sendText(msg.chat_id,msg.id, Get("Chats:Name:"..msg.content.text.text) , 'md', false, false, false, 0, nil, nil, nil)
     end
   end
@@ -343,8 +348,7 @@ if msg then
 if msg.content._ == "messageText" and  msg.content.text.text then
     if  Get('Stickers:'..msg.content.text.text)  then
 tdbot.deleteMessages(msg.chat_id,{[1] =msg.id})
- tdbot.sendSticker(msg.chat_id, (msg.reply_to_message_id or msg.id), Get('Stickers:'..msg.content.text.text)
-, false, true, nil, nil, nil)
+ tdbot.sendSticker(msg.chat_id, (msg.reply_to_message_id or msg.id), Get('Stickers:'..msg.content.text.text), false, true, nil, nil, nil)
 end
 
   if  Get('Gifs:'..msg.content.text.text)  then
